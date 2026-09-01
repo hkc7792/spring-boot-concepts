d2db2f135d40ca8d657f55a6de0e7aaf7c532dde
hkc7792 <hkc7792@gmail.com>
2026-09-01 08:54:58 +0530

    maven dependency understandings

---
+++ spring-boot-sivalabs/spring-boot-tips/src/main/java/com/app/tips/SpringBootTipsApplication.java
@@ -0,0 +1,13 @@
+package com.app.tips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTipsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-sivalabs/spring-boot-tips/src/test/java/com/app/tips/SpringBootTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
