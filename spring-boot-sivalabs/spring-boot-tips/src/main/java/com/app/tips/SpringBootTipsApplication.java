c8de59673af3608ea9ba6fc577a4a154199bbc67
Hariday Kannoujia <hkc7792@gmail.com>
2026-09-04 12:14:17 +0530

    added back junit dependency

--- spring-boot-sivalabs/spring-boot-tips/src/main/java/com/app/tips/SpringBootTipsApplication.java
+++ spring-boot-sivalabs/spring-boot-tips/src/main/java/com/app/tips/SpringBootTipsApplication.java
@@ -1,13 +1,19 @@
 package com.app.tips;
 
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class SpringBootTipsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootTipsApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTipsApplication.class, args);
+		logger.info("SpringBootTipsApplication started...");
+			SpringApplication.run(SpringBootTipsApplication.class, args);
 	}
 
 }
